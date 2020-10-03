b6860f4cd8b0a249928db5e64e568279a197cab6
Piotr Krupnik <piotr.krupnik@grupawp.pl>
2020-10-03 23:14:57 +0200

    set types

--- src/store/types.ts
+++ src/store/types.ts
@@ -1,3 +1,28 @@
 export const GET_WEATHER = 'GET_WETHER';
-export const SET_ERROR = 'SET_ERROR';
-export const SET_ALERT = 'SET_ALERT';
+// export const SET_ERROR = 'SET_ERROR';
+// export const SET_ALERT = 'SET_ALERT';
+
+export interface WeatherStation {
+  id: number;
+  stationName: string;
+  gegrLat: number;
+  gegrLon: number;
+  city: {
+    id: number;
+    name: string;
+    commune: {
+      communeName: string;
+      districtName: string;
+      provinceName: string;
+    }
+  }
+  addressStreet: boolean;
+}
+// export interface WeatherData {
+// }
+interface GetWeatherStation {
+  type: typeof GET_WEATHER;
+  payload: WeatherStation;
+}
+
+export type WeatherAction = GetWeatherStation;
